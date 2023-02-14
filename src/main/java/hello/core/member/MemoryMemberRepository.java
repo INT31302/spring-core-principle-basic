fc0580ecab27e3995efb9844dc7d66c808705719
INT31302 <tkdwo287@gmail.com>
2023-02-14 21:54:39 +0900

    feat: 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 동시성 이슈로 인해 ConcurrentHashMap 사용
+    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+
+    /**
+     * 멤버 저장 로직
+     * member 가 null 일 경우 return
+     * @param member 저장 하고자 하는 멤버 객체
+     */
+    @Override
+    public void save(Member member) {
+        if(member == null) return;
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
