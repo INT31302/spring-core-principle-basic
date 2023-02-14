e4ea23e3e82f63102676a0ed6c3f12baae8d163e
INT31302 <tkdwo287@gmail.com>
2023-02-14 23:56:14 +0900

    refactor: DIP 충족
    관심사의 분리 : 객체를 생성하고 연결하는 역할과 실행하는 역할이 명확히 분리되었다.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,12 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
+    private final MemberRepository memberRepository;
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
 
-    // 추상화에도 의존하고 구체화에도 의존
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
     public void join(Member member) {
