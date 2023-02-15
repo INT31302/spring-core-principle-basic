4aac40c47e8928b84333b99600f1b0fbd77abaa0
INT31302 <tkdwo287@gmail.com>
2023-02-15 23:57:41 +0900

    refactor: 중복 제거
    역할에 대한 구현체를 한눈에 볼 수 있도록 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,11 +10,29 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() {
+        return new MemberServiceImpl(MemberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    /**
+     * 역할에 대한 구현체 리턴
+     *
+     * @return MemberRepository
+     */
+    private MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+    }
+
+    /**
+     * 역할에 대한 구현체 리턴
+     *
+     * @return DiscountPolicy
+     */
+    private DiscountPolicy DiscountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
