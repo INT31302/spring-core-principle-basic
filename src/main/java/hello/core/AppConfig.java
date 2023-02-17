43da2f41c67b0493b2b424a45f8059dc07b95e4b
INT31302 <tkdwo287@gmail.com>
2023-02-17 23:57:38 +0900

    refactor: spring 으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,14 +2,22 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * Application 구성 정보 담당 하는 Configuration Class
+ */
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(MemberRepository());
     }
@@ -19,10 +27,12 @@ public class AppConfig {
      *
      * @return MemberRepository
      */
-    private MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
     }
@@ -32,7 +42,10 @@ public class AppConfig {
      *
      * @return DiscountPolicy
      */
-    private DiscountPolicy DiscountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy DiscountPolicy() {
+//        구현체 변경
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
